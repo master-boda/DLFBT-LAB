289b1a076b87769094e6376840255f4d78b3ede0
luisferuam <luis.lago@uam.es>
2020-09-10 23:24:09 +0200

    Update dlfbt.py

--- dlfbt.py
+++ dlfbt.py
@@ -41,20 +41,6 @@ class DataGeneratorLogistic1D(DataGeneratorLinear1D):
         DataGeneratorLinear1D.create_dataset(self, xmin, xmax, 0.0, n, seed)
         self.t = sigmoid(self.t) > np.random.rand(n, 1)
         self.modely = sigmoid(self.modely)
-"""
-    def plot_dataset(self, include_generator=True, estimation=None):
-        plt.figure(figsize=(6, 6))
-        plt.plot(self.x, self.t, 'o', label='data points')
-        if include_generator:
-            plt.plot(self.modelx, self.modely, 'r-', label='true model')
-        if estimation is not None:
-            plt.plot(estimation[0], estimation[1], 'm-', label='estimation')
-        plt.grid(True)
-        plt.xlabel("x")
-        plt.ylabel("t")
-        plt.legend()
-        plt.show()
-"""
 
 #-----------------------------------------------------------------------------------
 class DataGeneratorLinear(object):
@@ -70,3 +56,12 @@ class DataGeneratorLinear(object):
         self.t = np.dot(self.x, self.a) + self.b + np.random.randn(n, 1)*noise
         self.modelx = np.arange(xmin, xmax, (xmax-xmin)/100.0)[:, None]
         #self.modely = self.a*self.modelx + self.b
+
+#-----------------------------------------------------------------------------------
+class DataGeneratorLogistic(DataGeneratorLinear):
+    def __init__(self, a=[2.0], b=-10.0):
+        DataGneratorLinear.__init__(self, a, b)
+
+    def create_dataset(self, xmin=0.0, xmax=10.0, noise=2.0, n=100, seed=None):
+        DataGeneratorLinear.create_dataset(self, xmin, xmax, noise, n, seed)
+        self.t = sigmoid(self.t) > np.random.rand(n, 1)
