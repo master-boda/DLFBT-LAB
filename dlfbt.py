84ec32b1c69414392f8a5fa810623e851a028a03
luisferuam <luis.lago@uam.es>
2025-09-11 08:40:30 +0200

    test

--- dlfbt.py
+++ dlfbt.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 #-----------------------------------------------------------------------------------
 def sigmoid(z):
         return 1.0/(1.0+np.exp(-z))
